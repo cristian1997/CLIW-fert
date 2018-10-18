929af3fe10ba3f3572e283e09c97ec2e57114dfc
loghinalexandru <loghinalexandru61@gmail.com>
2018-10-18 21:34:10 +0300

    Styling

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -5,14 +5,13 @@ function next_image() {
     if(window.innerWidth < 700){
         return;
     }
-    console.log(slide_index);
     if (slide_index >= images.length) {
         slide_index = 0
     }
     images[slide_index].style.opacity = "0";
     images[(slide_index + 1) % images.length].style.opacity = "1";
     slide_index++;
-    timeout_function = setTimeout(next_image, 15000);
+    timeout_function = setTimeout(next_image, 10000);
 }
 
 next_image()

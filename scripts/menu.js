3a7de4e38c4ca31c79222160f179501917919fe5
loghinalexandru <loghinalexandru61@gmail.com>
2018-10-19 22:57:57 +0300

    Scaling/Html validation

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,14 +2,13 @@ var menu_content = document.getElementsByClassName("header__main-bar")[0].getEle
 var underline = menu_content.getElementsByTagName("hr");
 
 function display_menu() {
-
     if (menu_content.style.length > 0) {
         menu_content.style = null;
         for (let i = 0; i < underline.length; ++i) {
             underline[i].style.transform = "";
         }
     } else {
-        menu_content.style = "max-height: 120px;";
+        menu_content.style = "max-height: 200px;";
         for (let i = 0; i < underline.length; ++i) {
             underline[i].style.transform = "scale(1)";
         }

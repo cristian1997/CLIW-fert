d58f942d3d1a36031b384686b5cfc340d13e6954
loghinalexandru <loghinalexandru61@gmail.com>
2018-12-25 16:46:01 +0200

    SmallFix

--- scripts/auth/login.js
+++ scripts/auth/login.js
@@ -2,9 +2,7 @@ var loginButtons = document.getElementsByClassName("topic__list")[0];
 
 loginButtons.addEventListener("click", logUser);
 
-
 function getSiteFromString(string) {
-    console.log(string);
     if (string === "Server Fault") {
         return "serverfault";
     }
@@ -23,13 +21,7 @@ function getSiteFromString(string) {
 }
 
 function logUser(elem) {
-    if (elem.target.tagName === "DIV") {
-        title = elem.target.getElementsByTagName("h2")[0];
-        sessionStorage.setItem("site", getSiteFromString(title.innerHTML));
-    } else {
-        if (elem.target.tagName === "H2") {
-            sessionStorage.setItem("site", getSiteFromString(elem.target.innerHTML));
-        }
-    }
+    title = elem.target.getElementsByTagName("h2")[0];
+    sessionStorage.setItem("site", getSiteFromString(title.innerHTML));
     SE.authenticate("http://127.0.0.1:5500/login_succes.html");
 }

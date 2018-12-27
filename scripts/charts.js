70ad758e66535e396dc357db8f6b6f57e91fa4eb
loghinalexandru <loghinalexandru61@gmail.com>
2018-12-27 15:02:24 +0200

    Graphs

--- scripts/charts.js
+++ scripts/charts.js
@@ -3,11 +3,41 @@ var topicChart = document.getElementById("topic").getContext('2d');
 var answersChart = document.getElementById("answers").getContext('2d');
 var ratioChart = document.getElementById("ratio").getContext('2d');
 
-buildGraphs();
 
+function setPercentage(ratio) {
+    let elem = document.getElementById("percentage")
+    elem.innerHTML = ratio + "%";
+}
 SE.getBaseStats()
     .then((result) => {
-        buildRatio(result.up_vote_count + 1, result.down_vote_count + 1);
+        buildRatio(result.up_vote_count, result.down_vote_count);
+    }).catch((err) => {
+        showPopupError(err);
+    });
+
+SE.getTagsStats()
+    .then((result) => {
+        let tags = [];
+        let count = [];
+        for (let i = 0; i < result.length; ++i) {
+            tags.push(result[i].name.toUpperCase());
+            count.push(result[i].count);
+        }
+        buildFavoriteTags(tags, count);
+    }).catch((err) => {
+        showPopupError(err);
+    });
+
+SE.getAnswersStats()
+    .then((result) => {
+        let accepted = 0;
+        for (let i = 0; i < result.length; ++i) {
+            if (result[i].accepted) {
+                ++accepted;
+            }
+        }
+        buildCorrectAnswers(accepted, result.length - accepted);
+        setPercentage((accepted / result.length).toFixed(2) * 100);
     }).catch((err) => {
         showPopupError(err);
     });
@@ -21,12 +51,12 @@ function buildRatio(upvoteCount, downvoteCount) {
                 label: '# of Votes',
                 data: [upvoteCount, downvoteCount],
                 backgroundColor: [
-                    'rgba(0, 200, 0, 0.7)',
-                    'rgba(255, 0, 0, 0.7)',
+                    'rgba(0, 150, 0, 0.5)',
+                    'rgba(255, 0, 0, 0.5)',
                 ],
                 borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
+                    'rgba(0,0,0,0.1)',
+                    'rgba(0, 0, 0, 0.1)',
                 ],
                 borderWidth: 0,
             }]
@@ -42,57 +72,14 @@ function buildRatio(upvoteCount, downvoteCount) {
 
 }
 
-function buildGraphs() {
-    var correct = new Chart(correctChart, {
-        type: 'doughnut',
-        data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.7)',
-                    'rgba(54, 162, 235, 0.7)',
-                    'rgba(255, 206, 86, 0.7)',
-                    'rgba(75, 192, 192, 0.7)',
-                    'rgba(153, 102, 255, 0.7)',
-                    'rgba(255, 159, 64, 0.7)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true,
-                        fontColor: 'white'
-                    }
-                }]
-            },
-            legend: {
-                labels: {
-                    fontColor: 'white'
-                }
-            }
-        }
-    });
-
+function buildFavoriteTags(tags, count) {
     var topic = new Chart(topicChart, {
         type: 'line',
         data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            labels: tags,
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
+                label: '# of Tags',
+                data: count,
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.7)',
                     'rgba(54, 162, 235, 0.7)',
@@ -109,14 +96,59 @@ function buildGraphs() {
                     'rgba(153, 102, 255, 1)',
                     'rgba(255, 159, 64, 1)'
                 ],
-                borderWidth: 1
+                borderWidth: 1,
+                fill: false
             }]
         },
         options: {
             scales: {
                 yAxes: [{
                     ticks: {
-                        beginAtZero: true,
+                        fontColor: 'white',
+                        stepSize: 1
+                    }
+                }],
+                xAxes: [{
+                    ticks: {
+                        fontColor: 'white',
+                    }
+                }]
+            },
+            legend: {
+                labels: {
+                    fontColor: 'white',
+                }
+            }
+        }
+    });
+}
+
+function buildCorrectAnswers(accepted, pending) {
+    var correct = new Chart(correctChart, {
+        type: 'doughnut',
+        data: {
+            labels: ["Accepted", "Pending"],
+            datasets: [{
+                label: '# of Answers',
+                data: [accepted, pending],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.7)',
+                    'rgba(54, 162, 235, 0.7)',
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(0, 0, 0, 1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+
+        options: {
+            cutoutPercentage: 85,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        display: false,
                         fontColor: 'white'
                     }
                 }]
@@ -128,8 +160,11 @@ function buildGraphs() {
             }
         }
     });
+}
 
+buildGraphs();
 
+function buildGraphs() {
     var answers = new Chart(answersChart, {
         type: 'bar',
         data: {
@@ -172,8 +207,5 @@ function buildGraphs() {
             }
         }
     });
-
-
-
 
 }

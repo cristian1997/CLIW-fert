8ac1c4a51460f65ed5dd11d2bb802b74bd3f43bd
cristian1997 <cristian.vintur@gmail.com>
2019-01-24 12:37:26 +0200

    fb and twitter share

--- scripts/all.js
+++ scripts/all.js
@@ -1,3 +1,15 @@
-document.getElementById("dashboard-link").onclick = function (e) {
-	this.setAttribute("href", "./dashboard.html?account_id=" + sessionStorage.getItem("account_id") + "&" + "user_id=" + sessionStorage.getItem("user_id") + "&" + "site=" + sessionStorage.getItem("site"));
-};
+var dashboardParams = "account_id%3D" + sessionStorage.getItem("account_id");
+dashboardParams += "%26user_id%3D" + sessionStorage.getItem("user_id");
+dashboardParams += "%26site%3D" + sessionStorage.getItem("site");
+
+
+window.addEventListener("load", function() {
+	document.getElementById("dashboard-link").onclick = function (e) {
+		this.setAttribute("href", "./dashboard.html?" + dashboardParams);
+	};
+
+	document.getElementById("fb-share-button").href =
+		"https://www.facebook.com/sharer/sharer.php?u=https://127.0.0.1:5500/dashboard.html?" + dashboardParams;
+	document.getElementById("twitter-share-button").href =
+		"https://twitter.com/intent/tweet?text=https://127.0.0.1:5500/dashboard.html?" + dashboardParams;
+});
